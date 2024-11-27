4be4bace1a154e73e32daf20984fe89a07da3070
Naibitx <naibys.alzugarayofficial@gmail.com>
2024-11-26 20:28:27 -0500

    AudioControl is passed to main

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from Interface.main_interface import MainInterface
+from Controls.audio_control import AudioControl
+
 
 def main():
     root = tk.Tk()
     root.title("Data Acoustic Modeling Application")
     root.geometry("600x400")
 
-
-    interface = MainInterface(root)
+    audio_control = AudioControl()
+    interface = MainInterface(root, audio_control)
     root.mainloop()
 
 
