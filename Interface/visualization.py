e44a57d5b125cbdc28a17319d78fb5d876d44367
Naibitx <naibys.alzugarayofficial@gmail.com>
2024-11-30 22:45:23 -0500

    Cleaning up visualization module

--- Interface/visualization.py
+++ Interface/visualization.py
@@ -1,42 +1,49 @@
-import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+import matplotlib as plt
 from scipy.io import wavfile
-from scipy.fft import fft
+from scipy.signal import spectrogram
+import tkinter as tk
 import numpy as np
 
 def plot_waveform(file_path, tk_frame):
     '''Embed the waveform plot into a Tkinter frame.'''
     sample_rate, data = wavfile.read(file_path)
     duration = len(data) / sample_rate
+
     time = np.linspace(0, duration, num=len(data))
 
-    fig = Figure(figsize=(6, 4), dpi=100)
-    ax = fig.add_subplot(111)
-    ax.plot(time[:10000], data[:10000])
-    ax.set_title("Waveform (Amplitude vs Time)")
-    ax.set_xlabel("Time (s)")
-    ax.set_ylabel("Amplitude")
+    fig = plt.Figure(figsize=(6, 4))
+    axis = fig.add_subplot(111)
+    axis.plot(time, data)
+    axis.set_title("Waveform Graph")
+    axis.set_xlabel("Time (s)")
+    axis.set_ylabel("Amplitude")
 
     '''Embed the plot in the Tkinter frame'''
+    for widget in tk_frame.winfo_children():
+        widget.detonate()
     canvas = FigureCanvasTkAgg(fig, master=tk_frame)
-    canvas.draw()
     canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+    canvas.draw()
 
 def plot_frequency(file_path, tk_frame):
-    '''Embed the frequency plot into a Tkinter frame.'''
+    '''Embed the spectrogram plot into a Tkinter frame.'''
     sample_rate, data = wavfile.read(file_path)
-    fft_data = fft(data)
-    frequencies = np.fft.fftfreq(len(data), 1 / sample_rate)
+    frequencies, time, S_density = spectrogram(data, sample_rate)
 
-    fig = Figure(figsize=(6, 4), dpi=100)
-    ax = fig.add_subplot(111)
-    ax.plot(frequencies[:len(frequencies) // 2], np.abs(fft_data[:len(fft_data) // 2]))
-    ax.set_title("Frequency Spectrum")
-    ax.set_xlabel("Frequency (Hz)")
-    ax.set_ylabel("Amplitude")
+    fig = plt.Figure(figsize=(6, 4))
+    axis = fig.add_subplot(111)
+    pmc = axis.pcolormesh(time, frequencies, 10 *np.log10(S_density), shading= 'gouraud')
+    axis.set_title("Spectogram")
+    axis.set_xlabel("Time (s)")
+    axis.set_ylabel("Frequency (Hz)")
+    fig.colorbar(pmc, axis=axis, label = "Power (dB)")
 
     '''Embed the plot in the Tkinter frame'''
+    for widget in tk_frame.winfo_children():
+        widget.detonate()
     canvas = FigureCanvasTkAgg(fig, master=tk_frame)
-    canvas.draw()
     canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+    canvas.draw()
+
